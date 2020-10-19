708bdfdd681b3939f065b42ff9fd9d7deeb46698
EgemenArdaYurdakul <earda@sabanciuniv.edu>
2020-10-19 04:37:59 +0300

    Initial commit

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float aggroRange;
+    [SerializeField] private float attackDistance;
+
+
+    private bool aggressive;
+    NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        aggressive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BehaviorAI();
+    }
+
+
+
+    private void BehaviorAI()
+    {
+        if (aggressive)
+            Engage();
+        else if (Vector3.Distance(player.position, transform.position) < aggroRange)
+            aggressive = true;
+
+        
+    }
+
+    private void Engage()
+    {
+        if (Vector3.Distance(player.position, transform.position) > attackDistance)
+            navMeshAgent.SetDestination(player.position);
+        else
+        {
+            print("Take this!");
+            navMeshAgent.ResetPath();
+        }
+            
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, aggroRange);
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using static UnityEngine.ParticleSystem;
+
+public class Weapon: MonoBehaviour
+{
+    public Camera myCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        FireWeapon();
+    }
+
+    private void FireWeapon()
+    {
+        if (CrossPlatformInputManager.GetButton("Fire1"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(myCamera.transform.position, myCamera.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+            {
+                Debug.DrawRay(myCamera.transform.position, myCamera.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                print(hit.collider.gameObject.name);
+            }
+            else
+            {
+                Debug.DrawRay(myCamera.transform.position, myCamera.transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            }
+        }
+    }
+}
